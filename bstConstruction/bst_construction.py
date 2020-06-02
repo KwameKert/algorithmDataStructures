74fe12129a8bab63cdf92bfbf983b63eb5358d1c
kwamkert <tomriddle477@gmail.com>
2020-06-02 19:31:28 +0000

    lookup method added

--- bstConstruction/bst_construction.py
+++ bstConstruction/bst_construction.py
@@ -7,7 +7,9 @@ class BST:
 
     def insert(value):
         currentNode = self
+        #loop through tree to find the node to insert
         while currentNode:
+            #if currentNode left is none then insert a new node else assign currentNode left node to currentNode
             if value < currentNode.value:
                 if currentNode.left is None:
                     currentNode.left = BST(value)
@@ -15,9 +17,26 @@ class BST:
                 else:
                     currentNode = currentNode.left
             else:
+            #if currentNode right is none then insert a new node else assign currentNode right node to currentNode
                 if currentNode.right is None:
                     currentNode.right = BST(value)
                     break
                 else:
                     currentNode = currentNode.right
         return self
+
+    def lookup(value):
+        currentNode = self
+        #loop through tree
+        while currentNode:
+            #if value less than current node then reassign currentNode to currentNode left node
+            if value < currentNode.value:
+                currentNode = currentNode.left
+            #if value more than current node then reassign currentNode to currentNode right node
+            elif value> currentNode.value:
+                currentNode = currentNode.right
+            else:
+                return currentNode
+
+        #if node not found return False
+        return False
