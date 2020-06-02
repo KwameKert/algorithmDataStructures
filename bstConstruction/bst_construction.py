4e7d1b2ce34479990b29630ca12e2c01a0940c58
kwamkert <tomriddle477@gmail.com>
2020-06-02 19:20:18 +0000

    insert node

--- bstConstruction/bst_construction.py
+++ bstConstruction/bst_construction.py
@@ -1,9 +1,23 @@
-class Bst:
-
+class BST:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
 
-
+    def insert(value):
+        currentNode = self
+        while currentNode:
+            if value < currentNode.value:
+                if currentNode.left is None:
+                    currentNode.left = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.left
+            else:
+                if currentNode.right is None:
+                    currentNode.right = BST(value)
+                    break
+                else:
+                    currentNode = currentNode.right
+        return self
